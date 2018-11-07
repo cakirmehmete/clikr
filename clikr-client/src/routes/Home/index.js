246ddc5e2af5cc0c224f45315d49848353c8c9e0
Emre Cakir <cakirmehmete@gmail.com>
2018-11-07 00:29:30 -0500

    Created two routes for student and professor

--- clikr-client/src/routes/Home/index.js
+++ clikr-client/src/routes/Home/index.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
 import logo from '../../assets/logo.svg';
 import './style.css'; // Not our preferred way of importing style
-import Button from '../../components/Button'
+import Button from '@material-ui/core/Button'
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Button text="Test 123" />
+          <Button variant="contained" color="primary">
+            Go to Student Page
+          </Button>
+          <Button variant="contained" color="primary">
+            Go to Professor Page
+          </Button>
         </header>
       </div>
     );
