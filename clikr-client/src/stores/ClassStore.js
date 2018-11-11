923efcb6f0d876ac3ff7060891a3e6a26e4b6176
Emre Cakir <cakirmehmete@gmail.com>
2018-11-11 14:02:25 -0500

    Added action to the class store to add a new class

--- clikr-client/src/stores/ClassStore.js
+++ clikr-client/src/stores/ClassStore.js
@@ -1,3 +1,11 @@
+import {observable, action} from 'mobx-react';
+import {ClassObj} from '../models/ClassObj';
+
 export default class ClassStore {
-    @observable classes = [];
+    @observable classes = [new ClassObj("Test", "adasdasd")];
+
+    @action
+    addClass(classObj) {
+      this.classes.push(classObj)
+    }
   }
