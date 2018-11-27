b9a88f8d9a408f0656e35512c99c8b03331c83e8
Angela Feng <afeng@princeton.edu>
2018-11-26 21:38:15 -0500

    home button added, not linked yet

--- clikr-client/src/components/TopMenuBar/index.jsx
+++ clikr-client/src/components/TopMenuBar/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import ToolbarGroup from '@material-ui/core/Toolbar';
+import DropDownMenu from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import SvgIcon from '@material-ui/core/SvgIcon';
 
 const drawerWidth = 240;
 
@@ -21,11 +25,29 @@ class TopMenuBar extends React.Component {
 
     render() {
         return (
-            <AppBar position="static" color="primary" className={this.styles.appBar}>
+            <AppBar position="static" color="white" className={this.styles.appBar}>
                 <Toolbar>
-                    <Typography variant="h6" color="inherit">
-                        Professor Home Page
-                        </Typography>
+                    <ToolbarGroup firstChild={true} float="left">
+                        <IconButton color="primary">
+                            <SvgIcon>
+                                <path fill="#FFEA00"
+                                    d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" />
+                            </SvgIcon>
+                        </IconButton>
+                    </ToolbarGroup>
+
+                    <ToolbarGroup style={{
+                        float       : 'none',
+                        width       : '200px',
+                        marginLeft  : 'auto',
+                        marginRight : 'auto'
+                    }}>
+                    </ToolbarGroup>
+
+                    <ToolbarGroup lastChild={true} float="right">
+                        <DropDownMenu>
+                        </DropDownMenu>
+                    </ToolbarGroup>
                 </Toolbar>
             </AppBar>
 
