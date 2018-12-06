54e84207bb8f826544eb7922446581fad3959dac
Emre Cakir <cakirmehmete@gmail.com>
2018-12-06 03:54:07 -0500

    Fixed deployment

---
+++ app/react_app/build/precache-manifest.590c820b5ad7597ac738bdff9f076393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c2368b5735d1be0b97a9e66a1af7866",
+    "url": "/static/media/clikrlogo.0c2368b5.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac1dfe2f8913d4145f9f",
+    "url": "/static/js/main.ac1dfe2f.chunk.js"
+  },
+  {
+    "revision": "d43f5f2083a9f2d94a0f",
+    "url": "/static/js/1.d43f5f20.chunk.js"
+  },
+  {
+    "revision": "ac1dfe2f8913d4145f9f",
+    "url": "/static/css/main.5ed117ae.chunk.css"
+  },
+  {
+    "revision": "df02f338755d85c79e7af80e6354768c",
+    "url": "/index.html"
+  }
+];
