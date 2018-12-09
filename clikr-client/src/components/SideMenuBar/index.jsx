60935c19d873654046d5197b9ed0be9420d8ad6f
Zoe Yang <zsyang@princeton.edu>
2018-12-09 14:35:49 -0500

    updated colors for side menu bar

--- clikr-client/src/components/SideMenuBar/index.jsx
+++ clikr-client/src/components/SideMenuBar/index.jsx
@@ -8,7 +8,9 @@ import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import ListItem from '@material-ui/core/ListItem';
 import { observer, inject } from 'mobx-react';
-import logo from '../../assets/clikrlogo.png';
+import logo from '../../assets/clikrlogo2.png';
+import Typography from '@material-ui/core/Typography';
+
 const drawerWidth = 240;
 
 const styles = theme => ({
@@ -16,8 +18,17 @@ const styles = theme => ({
         width: drawerWidth,
         flexShrink: 0,
     },
+    listtext: {
+        color: theme.palette.primary.main
+    },
     drawerPaper: {
         width: drawerWidth,
+        backgroundColor: theme.palette.secondary.main,
+    },
+    hover: {
+        "&:hover": {
+            backgroundColor: theme.palette.secondary.dark
+          }
     },
     toolbar: theme.mixins.toolbar,
 });
@@ -52,8 +63,10 @@ class SideMenuBar extends React.Component {
                 <Divider />
                 <List>
                     {this.profStore.courses.map((courseObj, index) => {
-                        return (<ListItem button key={index} onClick={() => this.handleClick(courseObj.id)}>
-                            <ListItemText primary={courseObj.title} />
+                        return (<ListItem className={this.styles.hover} button key={index} onClick={() => this.handleClick(courseObj.id)}>
+                            <ListItemText
+                            disableTypography
+                            primary={<Typography type="body2" className={this.styles.listtext}>{courseObj.title}</Typography>} />
                         </ListItem>)
                     })}
                 </List>
