132a9eb94aa35b83d0d5f4b06442c02dd6ee6fc5
Zoe Yang <zsyang@princeton.edu>
2018-12-24 04:16:41 -0500

    added calendar date picker and lecture title defaults to lecture number

--- clikr-client/src/routes/Professor/AddLecture/index.jsx
+++ clikr-client/src/routes/Professor/AddLecture/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { Redirect } from "react-router-dom";
@@ -16,6 +17,12 @@ const styles = theme => ({
         display: 'flex',
         flexWrap: 'wrap',
     },
+    gridItem: {
+        margin: theme.spacing.unit
+    },
+    gridItemButton: {
+        margin: theme.spacing.unit*2
+    },
     textField: {
         marginLeft: theme.spacing.unit,
         marginRight: theme.spacing.unit,
@@ -23,23 +30,51 @@ const styles = theme => ({
     },
 });
 
+@inject("profStore")
 @inject("apiService")
 class ProfessorAddLecture extends React.Component {
     constructor(props) {
         super(props)
         this.styles = props.classes
-
-        const { courseId } = this.props.match.params
+        this.profStore = props.profStore
+        this.courseId = this.props.match.params.courseId
+        
+        const date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        if (month.toString().length === 1) {
+            month = "0" + month.toString();
+        }
+        else {
+            month = month.toString();
+        }
+        let day = date.getDate();
+        if (day.toString().length === 1) {
+            day = "0" + day.toString();
+        } 
+        else {
+            day = day.toString();
+        }
         this.state = {
             toLecture: false,
-            title: '',
-            date: '',
-            courseId: courseId,
+            title: "Lecture ",
+            courseId: this.courseId,
+            numLects: this.props.location.state.numLects.toString(),
             errors: {title: ''},
             titleValid: true,
-            formValid: false
+            formValid: false,
+            date: year + "-" + month + "-" + day
         }
     }
+
+    componentDidMount() {
+        const lectString = "Lecture " + this.state.numLects.toString();
+        this.setState({
+            title: lectString
+        })
+        this.handleValidation()
+    }
+
 
     handleValidation() {
         let titleValid = this.state.titleValid;
@@ -60,6 +95,12 @@ class ProfessorAddLecture extends React.Component {
         this.setState({formValid: this.state.titleValid });
     }
 
+
+    handleDate = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }  
 
     handleChange = name => event => {
         this.setState({
@@ -83,37 +124,51 @@ class ProfessorAddLecture extends React.Component {
 
         return (
             <div className={this.styles.paper}>
-                <Typography variant="h6" color="textPrimary">
-                    Add New Lecture:
-                </Typography>
-                <form className={this.styles.container} noValidate autoComplete="off">
-                    <TextField
-                        required
-                        error={!this.state.titleValid}
-                        id="standard-name"
-                        label="Lecture Title"
-                        className={this.styles.textField}
-                        value={this.state.title}
-                        onChange={this.handleChange('title')}
-                        margin="normal"
-                        helperText={this.state.errors["title"]}
-                    />
-                    <TextField
-                        id="standard-name"
-                        label="Lecture Date"
-                        className={this.styles.textField}
-                        value={this.state.date}
-                        onChange={this.handleChange('date')}
-                        margin="normal"
-                    />
-                    <Button
-                        disabled={!this.state.formValid}
-                        variant="outlined"
-                        color="primary"
-                        onClick={this.handleSubmit}>
-                            Submit
-                    </Button>
-                </form>
+                <Grid container direction="column">
+                    <Grid item className={this.styles.gridItem}>
+                        <Typography variant="h6" color="textPrimary">
+                            Add New Lecture:
+                        </Typography>
+                    </Grid>
+                    <form noValidate autoComplete="off">
+                    <Grid item className={this.styles.gridItem}>
+                        <TextField
+                            requiredtitle="true"
+                            error={!this.state.titleValid}
+                            id="standard-name"
+                            label="Lecture Title"
+                            className={this.styles.textField}
+                            value={this.state.title}
+                            onChange={this.handleChange('title')}
+                            margin="normal"
+                            helperText={this.state.errors["title"]}
+                        />
+                    </Grid>
+                    <Grid item className={this.styles.gridItem}>
+                        <TextField
+                            id="date"
+                            className={this.styles.textField}
+                            name='date'
+                            onChange={e => this.handleDate(e)}
+                            type="date"
+                            defaultValue={this.state.date}
+                            InputLabelProps={{
+                            shrink: true,
+                            }}
+                        />
+                    </Grid>
+                    <Grid item className={this.styles.gridItemButton}>
+                        <Button
+                            disabled={!this.state.formValid}
+                            variant="outlined"
+                            color="primary"
+                            className={this.styles.container}
+                            onClick={this.handleSubmit}>
+                                Submit
+                        </Button>
+                   </Grid>
+                    </form>
+                </Grid>
             </div>
         );
     }
