50c86f65621e4095ef2645caf633ecf81325e328
Zoe Yang <zsyang@princeton.edu>
2018-12-24 04:20:39 -0500

    moved lectures list to its on component for clarity

---
+++ clikr-client/src/components/ListOfAllLectures/index.jsx
@@ -0,0 +1,58 @@
+
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import { observer } from 'mobx-react';
+import LectListItemNavEdit from '../LectListItemNavEdit';
+
+
+
+const styles = theme => ({
+    icon: {
+        color: theme.palette.primary.light
+    }
+});
+
+@observer
+class ListOfAllLectures extends React.Component {
+    state = {
+        lectures: []
+    }
+
+    constructor(props) {
+        super(props)
+        this.styles = props.classes
+        this.profStore = props.profStore
+        this.courseId  = props.courseId
+        this.apiProfService = props.apiProfService
+    }
+    componentDidMount() {
+        if (this.courseId !== undefined) {
+            this.setState({
+                lectures: this.profStore.getCourseLectures(this.courseId)
+            })
+        }
+        
+    }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.courseId !== undefined) {
+            this.setState({
+                lectures: this.profStore.getCourseLectures(nextProps.courseId)
+            })
+        }
+    }
+
+    render() {
+        return (
+            <List component="nav">
+                {this.state.lectures.map((lectureObj, index) => {
+                    return (
+                        <LectListItemNavEdit key={index} profStore={this.profStore} apiProfService={this.apiProfService} lectureId={lectureObj.id} lectureTitle={lectureObj.title} />
+                    )
+                })}
+            </List>
+        );
+    }
+}
+
+export default withStyles(styles)(ListOfAllLectures);
