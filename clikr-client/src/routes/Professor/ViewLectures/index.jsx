275ef381f4be95af894c983c75233ebf48e7845b
Zoe Yang <zsyang@princeton.edu>
2018-12-24 04:16:59 -0500

    modified edit and deletion to occur in place

--- clikr-client/src/routes/Professor/ViewLectures/index.jsx
+++ clikr-client/src/routes/Professor/ViewLectures/index.jsx
@@ -3,22 +3,28 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import AllLecturesFrame from '../../../components/AllLecturesFrame';
 import TopLecturesFrame from '../../../components/TopLecturesFrame';
+import APIProfService from '../../../services/APIProfService';
+import { observer, inject } from 'mobx-react';
 
 const styles = theme => ({
 
 });
 
+@inject("profStore")
+@observer
 class ProfessorViewLectures extends React.Component {
     constructor(props) {
         super(props)
         this.styles = props.classes
+        this.profStore = props.profStore
+        this.apiProfService = new APIProfService(this.profStore)
     }
 
     render() {
         return (
             <Grid container spacing={24}>
                 <Grid item xs={8}>
-                    <AllLecturesFrame courseId={this.props.match.params.courseId} />
+                    <AllLecturesFrame profStore={this.profStore} apiProfService={this.apiProfService} courseId={this.props.match.params.courseId} />
                 </Grid>
                 <Grid item xs={4}>
                     <TopLecturesFrame />
