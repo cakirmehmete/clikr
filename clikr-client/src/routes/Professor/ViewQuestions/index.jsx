89ff4b3fb5a924aa8d63d7380b7463985306f052
Zoe Yang <zsyang@princeton.edu>
2019-01-08 22:18:58 -0500

    button fixes

--- clikr-client/src/routes/Professor/ViewQuestions/index.jsx
+++ clikr-client/src/routes/Professor/ViewQuestions/index.jsx
@@ -104,6 +104,10 @@ class ProfessorViewQuestions extends React.Component {
                 currentQuestionId: this.convertQuestionIndexToId(this.state.currentQuestionIndex)
             })
         }
+        if (this.state.currentQuestionId !== this.convertQuestionIndexToId(this.state.currentQuestionIndex))
+            this.setState({
+                currentQuestionId: this.convertQuestionIndexToId(this.state.currentQuestionIndex)
+            })
     }
 
     componentWillUnmount() {
@@ -213,7 +217,7 @@ class ProfessorViewQuestions extends React.Component {
                     <Typography variant="h4" component="h2" className={this.styles.textQ} align="center">
                         Q{this.convertQuestionIdToIndex(this.state.currentQuestionId) + 1}: {this.state.parentLecture.title}
                     </Typography>
-                    <Button variant="outlined" color="primary" onClick={() => this.handleBtnClick()} className={this.styles.startLectureBtn} disabled={this.state.btnStatus === 3}>
+                    <Button variant="outlined" color="primary" onClick={() => this.handleBtnClick()} className={this.styles.startLectureBtn} disabled={this.state.btnStatus === 3 || this.state.parentLecture.questions.length === 0}>
                         {this.state.btnStatus === 0 ? "Open Question " + (this.convertQuestionIdToIndex(this.state.currentQuestionId) + 1) :
                             this.state.btnStatus === 1 ? "Close Question " + (this.convertQuestionIdToIndex(this.state.currentQuestionId) + 1) :
                                 this.state.btnStatus === 2 ? "Open Question " + (this.convertQuestionIdToIndex(this.state.currentQuestionId) + 2) :
