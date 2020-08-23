601b50b8a513a3ff7c026a2efc6bcd34e9cec4c6
christopher ye <czye@princeton.edu>
2020-08-23 13:34:40 -0400

    changed

--- clikr-client/src/routes/Login/index.jsx
+++ clikr-client/src/routes/Login/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import { Provider } from 'mobx-react';
+import { baseURL } from '../../constants/api';
 import { withStyles } from '@material-ui/core/styles';
 import LoginProf from "./LoginProf"
 import CreateProf from "./CreateProf"
@@ -18,18 +19,23 @@ class LoginRoutes extends Component {
         this.apiLoginService = new APILoginService()
     }
 
+    componentDidMount() {
+        var url_add_on_1 = 'student/login?service=';
+        var url_add_on_2 = '/student';
+        var end_sub = 14;
+        if (window.location.href.split('login/')[1].valueOf() === 'prof'.valueOf()) {
+            url_add_on_1 = 'professor/login?service=';
+            url_add_on_2 = '/professor';
+            end_sub = 11;
+        }
+
+        window.location.replace(baseURL + url_add_on_1 + window.location.href.substring(0, window.location.href.length - end_sub) + url_add_on_2);
+    }
+
     render() {
         return(
             <div>
-                <Provider apiLoginService={this.apiLoginService}>
-                    <Switch>
-                        <Route path='/login/prof' component={LoginProf} />
-                        <Route path='/login/student' component={LoginStudent} />
-                        <Route path='/login/create-prof' component={CreateProf} />
-                        <Route path='/login/create-student' component={CreateStudent} />
-                        <Route component={NoMatch} />
-                    </Switch>
-                </Provider>
+                Logging In...
             </div>
         )
     }
