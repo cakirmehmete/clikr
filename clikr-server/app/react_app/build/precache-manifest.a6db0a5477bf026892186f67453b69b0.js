e235467b05b0dfd658e660cef4c27c866be7ea3f
Emre Cakir <cakirmehmete@gmail.com>
2020-08-23 12:49:09 -0400

    Updated server with new build of react app

---
+++ clikr-server/app/react_app/build/precache-manifest.a6db0a5477bf026892186f67453b69b0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "db710142fa8c5c86a7af",
+    "url": "/static/css/main.02666e7c.chunk.css"
+  },
+  {
+    "revision": "db710142fa8c5c86a7af",
+    "url": "/static/js/main.db710142.chunk.js"
+  },
+  {
+    "revision": "a570780e2e958d6fd0a8",
+    "url": "/static/js/1.a570780e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f8a6c63b9872a153a3b6d33d63b92367",
+    "url": "/static/media/clikrlogo2.f8a6c63b.png"
+  },
+  {
+    "revision": "6c47d62cc418b4e9b93842ae7772f3f6",
+    "url": "/static/media/clikrlogo.6c47d62c.png"
+  },
+  {
+    "revision": "b3b26c463843c3ee0183d58e569c247e",
+    "url": "/static/media/holder_hall.b3b26c46.jpg"
+  },
+  {
+    "revision": "8125ca7f2e14d944cb3012299c91d0c7",
+    "url": "/static/media/nassau_hall.8125ca7f.jpg"
+  },
+  {
+    "revision": "d8b36ae391180bbf7a220aef81898806",
+    "url": "/static/media/boathouse.d8b36ae3.jpeg"
+  },
+  {
+    "revision": "0f558cafc6180032e227e70066544fec",
+    "url": "/index.html"
+  }
+];
