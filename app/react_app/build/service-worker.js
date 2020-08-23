a7c9337838c52ceeb5584456167f2b404db9cad3
Emre Cakir <cakirmehmete@gmail.com>
2020-08-23 12:49:09 -0400

    Updated server with new build of react app

--- app/react_app/build/service-worker.js
+++ app/react_app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e8c643843cfdcf7340b7c12ceba4e75.js"
+  "/precache-manifest.a6db0a5477bf026892186f67453b69b0.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
