a1ff8adf11f2be420eab3b2152225379da5e108f
Emre Cakir <cakirmehmete@gmail.com>
2020-08-23 15:57:13 -0400

    updated build

---
+++ clikr-server/app/react_app/build/precache-manifest.4610970d0b0ce2e3db89b2b430163b2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c796a3854aa0b0f517f6",
+    "url": "/static/css/main.bcf447f1.chunk.css"
+  },
+  {
+    "revision": "c796a3854aa0b0f517f6",
+    "url": "/static/js/main.c796a385.chunk.js"
+  },
+  {
+    "revision": "6f867e4284b31ab42841",
+    "url": "/static/js/1.6f867e42.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f8a6c63b9872a153a3b6d33d63b92367",
+    "url": "/static/media/clikrlogo2.f8a6c63b.png"
+  },
+  {
+    "revision": "6c47d62cc418b4e9b93842ae7772f3f6",
+    "url": "/static/media/clikrlogo.6c47d62c.png"
+  },
+  {
+    "revision": "d8b36ae391180bbf7a220aef81898806",
+    "url": "/static/media/background.d8b36ae3.jpeg"
+  },
+  {
+    "revision": "22a85d56740dceb491a48f9816cd1908",
+    "url": "/index.html"
+  }
+];
