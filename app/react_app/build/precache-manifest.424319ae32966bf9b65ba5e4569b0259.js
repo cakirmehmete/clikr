888bf46e97cc0981f5f09461640c8ef81a313af3
Emre Cakir <cakirmehmete@gmail.com>
2020-08-24 19:35:19 -0400

    Update Build and Deploy

---
+++ app/react_app/build/precache-manifest.424319ae32966bf9b65ba5e4569b0259.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f242c9b759cc7ffaa44a",
+    "url": "/static/css/main.bb19f4db.chunk.css"
+  },
+  {
+    "revision": "f242c9b759cc7ffaa44a",
+    "url": "/static/js/main.f242c9b7.chunk.js"
+  },
+  {
+    "revision": "7cbfb0d06f65d0b54cdc",
+    "url": "/static/js/1.7cbfb0d0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f8a6c63b9872a153a3b6d33d63b92367",
+    "url": "/static/media/clikrlogo2.f8a6c63b.png"
+  },
+  {
+    "revision": "6c47d62cc418b4e9b93842ae7772f3f6",
+    "url": "/static/media/clikrlogo.6c47d62c.png"
+  },
+  {
+    "revision": "d8b36ae391180bbf7a220aef81898806",
+    "url": "/static/media/background.d8b36ae3.jpeg"
+  },
+  {
+    "revision": "46ec076535a87f20e8e36c10fbec49bf",
+    "url": "/index.html"
+  }
+];
