2bcd5e108803685e35a5d8cb322d879275d77bc3
Emre Cakir <cakirmehmete@gmail.com>
2020-08-24 19:13:28 -0400

    Custom URL
    Allows for the staging

--- clikr-client/src/constants/api.js
+++ clikr-client/src/constants/api.js
@@ -1,5 +1,8 @@
+const windowSplit = window.location.href.split("/")
+const base = windowSplit[0] + "//" + windowSplit[2]
+
 export const socketioURL =
          process.env.NODE_ENV === "production"
-           ? "https://clikr.herokuapp.com/"
+           ? base
            : "http://localhost:5000/";
 export const baseURL = socketioURL + "api/v1/"; 
